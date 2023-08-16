ed5f6cd0af7b6d1d479b86528750a2e908a03884
liutao <tao.liu@enabot.com>
2023-08-16 17:31:44 +0800

    compose constraint layout

--- composesamples/src/main/java/com/enabot/composesamples/ui/MainActivity.kt
+++ composesamples/src/main/java/com/enabot/composesamples/ui/MainActivity.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
@@ -47,17 +49,23 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import com.enabot.composesamples.R
 import com.enabot.composesamples.ui.theme.JetpackrelearnTheme
 import com.enabot.composesamples.ui.theme.welcomeAssets
+import kotlin.contracts.contract
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -255,3 +263,152 @@ fun PreMessageCard() {
     }
 
 }
+
+/**
+ * 1.约束布局ConstraintLayout
+ * 创建引用的方式 createRefs() createRefFor()
+ *
+ * 2.Guideline
+ * 可以创建一个指示线，指示线可以相较于父组件的top、bottom、start、end以一个百分比或dp进行偏移，
+ * 以便别的组件可以针对指示线进行约束，Guideline创建方式有以下4种：
+ * - 较于父组件左边10%位置创建
+ *
+ * `val startGuideline = createGuidelineFromStart(0.1f)`
+ * - 较于父组件右边10%位置创建
+ *
+ * `val endGuideline = createGuidelineFromEnd(0.1f)`
+ *
+ * - 较于父组件顶部16dp位置创建
+ *
+ * `val topGuideline = createGuidelineFromTop(16.dp)`
+ *
+ * - 较于父组件底部16dp位置创建
+ *
+ * `val bottomGuideline = createGuidelineFromBottom(16.dp)`
+ *
+ * 3.Barrier
+ * Barrier可以将多个内容组件引用组合成一个屏障，其他的组件就可以以屏障Barrier来进行约束，
+ * 创建Barrier有以下4中方式:
+ * - 以btn，txt进行组合，创建右边的barrier
+ *
+ *  `val barrier = createEndBarrier(btn, txt)`
+ *
+ * - 以btn，txt进行组合，创建左边的barrier
+ *
+ *  `val barrier = createStartBarrier(btn, txt)`
+ *
+ * - 以btn，txt进行组合，创建顶部的barrier
+ *
+ *  `val barrier = createTopBarrier(btn, txt)`
+ *
+ * - 以btn，txt进行组合，创建底部的barrier
+ *
+ *  `val barrier = createBottomBarrier(btn, txt)`
+ *
+ *  4.Chain
+ *  Chain用于将多个内容组件引用组合成以个链，并以不同的 ChainStyles 配置链内各个组件的分布，创建方式有两种：
+ *  - 创建水平的链
+ *
+ * val chain = createHorizontalChain(txt1, txt2, txt3, chainStyle = ChainStyle.SpreadInside)
+ *
+ * - 创建垂直的链
+ *
+ * val chain = createVerticalChain(txt1, txt2, txt3, chainStyle = ChainStyle.SpreadInside)
+ * ChainStyles配置:
+ * - ChainStyle.Spread：空间会在所有可组合项之间均匀分布，包括第一个可组合项之前和最后一个可组合项之后的可用空间。
+ * - ChainStyle.SpreadInside：空间会在所有可组合项之间均匀分布，不包括第一个可组合项之前或最后一个可组合项之后的任何可用空间。
+ * - ChainStyle.Packed：空间会分布在第一个可组合项之前和最后一个可组合项之后，各个可组合项之间没有空间，会挤在一起。
+ */
+@Preview(name = "Light Mode")
+@Composable
+fun ConstraintLayoutTest() {
+    Surface(modifier = Modifier.fillMaxWidth()) {
+        ConstraintLayout {
+            val (button1, button2) = createRefs()
+            val startGuideline = createGuidelineFromStart(0.5f)
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.constrainAs(button1) {
+                    top.linkTo(parent.top, margin = 16.dp)
+                    end.linkTo(startGuideline)
+                }) {
+                Text(text = "button1")
+            }
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.constrainAs(button2) {
+                    top.linkTo(parent.top, margin = 8.dp)
+                    start.linkTo(startGuideline)
+                }) {
+                Text(text = "button2")
+            }
+            val barrier = createBottomBarrier(button1, button2)
+            val button3 = createRef()
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.constrainAs(button3) {
+                    top.linkTo(barrier)
+                    start.linkTo(parent.start)
+                }) {
+                Text(text = "button3")
+            }
+            val (button4,button5) = createRefs()
+            val chain = createHorizontalChain(button3,button4,button5, chainStyle = ChainStyle.SpreadInside)
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.constrainAs(button4){
+                top.linkTo(button3.top)
+            }) {
+                Text(text = "button4")
+            }
+            Button(onClick = { /*TODO*/ },modifier = Modifier.constrainAs(button5){
+                top.linkTo(button3.top)
+            }) {
+                Text(text = "button5")
+            }
+        }
+    }
+}
+
+/**
+ * 如果你不想在ConstraintLayout作用域内定义引用以及约束规则，
+ * 那么可以将 ConstraintSet 作为参数传递给 ConstraintLayout，
+ * 外部通过createRefFor("key")指定一个字符串key创建引用，constrain("key")进行约束条件；
+ * 内容组件使用修饰符layoutId("key")进行约束匹配
+ */
+@Preview
+@Composable
+fun ConstraintLayoutTest2() {
+    //假如布局需要根据尺寸和方向来更改布局内容，则BoxWithConstraints自适应布局可以派上用场。
+    BoxWithConstraints {
+        val constraints = if (minWidth < 600.dp) {
+            decoupledConstraints(margin = 16.dp) // Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) // Landscape constraints
+        }
+        ConstraintLayout(constraintSet = constraints) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.layoutId("button1")
+            ) {
+                Text(text = "button1")
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.layoutId("button2")
+            ) {
+                Text(text = "button2")
+            }
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button1 = createRefFor("button1")
+        val button2 = createRefFor("button2")
+        constrain(button1) {
+            top.linkTo(parent.top, margin)
+        }
+        constrain(button2) {
+            top.linkTo(button1.bottom)
+            start.linkTo(button1.end)
+        }
+    }
+}
+
--- composesamples/src/main/java/com/enabot/composesamples/ui/theme/Theme.kt
+++ composesamples/src/main/java/com/enabot/composesamples/ui/theme/Theme.kt
@@ -70,10 +70,10 @@ fun JetpackrelearnTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
